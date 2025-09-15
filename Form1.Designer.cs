82b9c024232bfbf923d2cfb91ceda11949bd8101
Faezeh Nikbakht <nikbakhtfaeze@gmail.com>
2025-09-15 11:30:58 +0200

    Initial commit - WinForms control app

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace LEDdriverControlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbPorts = new ComboBox();
+            btnConnect = new Button();
+            btnRead = new Button();
+            lblVout = new Label();
+            lblIout = new Label();
+            lblDACValue = new TextBox();
+            btnSetCurrent = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            btnLedToggle = new Button();
+            trackBarDAC = new TrackBar();
+            trackBarDAC.Minimum = 0;
+            trackBarDAC.Maximum = 100;
+            trackBarDAC.TickFrequency = 10; // هر 10 تا یک خط نشون بده (اختیاری)
+
+            ((System.ComponentModel.ISupportInitialize)trackBarDAC).BeginInit();
+            SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            cmbPorts.FormattingEnabled = true;
+            cmbPorts.Location = new Point(219, 80);
+            cmbPorts.Name = "cmbPorts";
+            cmbPorts.Size = new Size(182, 33);
+            cmbPorts.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(724, 62);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(215, 51);
+            btnConnect.TabIndex = 1;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(724, 131);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(215, 45);
+            btnRead.TabIndex = 2;
+            btnRead.Text = "Read";
+            btnRead.UseVisualStyleBackColor = true;
+            // 
+            // lblVout
+            // 
+            lblVout.AutoSize = true;
+            lblVout.Location = new Point(219, 151);
+            lblVout.Name = "lblVout";
+            lblVout.Size = new Size(49, 25);
+            lblVout.TabIndex = 3;
+            lblVout.Text = "Vout";
+            // 
+            // lblIout
+            // 
+            lblIout.AutoSize = true;
+            lblIout.Location = new Point(458, 151);
+            lblIout.Name = "lblIout";
+            lblIout.Size = new Size(44, 25);
+            lblIout.TabIndex = 4;
+            lblIout.Text = "Iout";
+            // 
+            // lblDACValue
+            // 
+            lblDACValue.Location = new Point(742, 342);
+            lblDACValue.Name = "lblDACValue";
+            lblDACValue.Size = new Size(150, 31);
+            lblDACValue.TabIndex = 5;
+
+            // 
+            // btnSetCurrent
+            // 
+            btnSetCurrent.Location = new Point(724, 195);
+            btnSetCurrent.Name = "btnSetCurrent";
+            btnSetCurrent.Size = new Size(215, 41);
+            btnSetCurrent.TabIndex = 6;
+            btnSetCurrent.Text = "Set Current";
+            btnSetCurrent.UseVisualStyleBackColor = true;
+            // 
+            // btnLedToggle
+            // 
+            btnLedToggle.Location = new Point(724, 265);
+            btnLedToggle.Name = "btnLedToggle";
+            btnLedToggle.Size = new Size(216, 41);
+            btnLedToggle.TabIndex = 7;
+            btnLedToggle.Text = "LED ";
+            btnLedToggle.UseVisualStyleBackColor = true;
+            btnLedToggle.Click += btnLedToggle_Click;
+            // 
+            // trackBarDAC
+            // 
+            trackBarDAC.Location = new Point(135, 342);
+            trackBarDAC.Name = "trackBarDAC";
+            trackBarDAC.Size = new Size(524, 69);
+            trackBarDAC.TabIndex = 8;
+            trackBarDAC.Scroll += trackBarDAC_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1059, 510);
+            Controls.Add(trackBarDAC);
+            Controls.Add(btnLedToggle);
+            Controls.Add(btnSetCurrent);
+            Controls.Add(lblDACValue);
+            Controls.Add(lblIout);
+            Controls.Add(lblVout);
+            Controls.Add(btnRead);
+            Controls.Add(btnConnect);
+            Controls.Add(cmbPorts);
+            Name = "Form1";
+            Text = "LED Driver Controler";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)trackBarDAC).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbPorts;
+        private Button btnConnect;
+        private Button btnRead;
+        private Label lblVout;
+        private Label lblIout;
+        private TextBox lblDACValue;
+        private Button btnSetCurrent;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Button btnLedToggle;
+        private TrackBar trackBarDAC;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LEDdriverControlApp
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort1 = new SerialPort();
+        bool ledState = false; // false = OFF, true = ON
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshPorts();
+
+        }
+
+        private void RefreshPorts()
+        {
+            cmbPorts.Items.Clear();
+            cmbPorts.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (cmbPorts.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a COM port first.");
+                return;
+            }
+
+            try
+            {
+                serialPort1.PortName = cmbPorts.SelectedItem.ToString();
+                serialPort1.BaudRate = 115200;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+
+                serialPort1.Open();
+                MessageBox.Show("Connected to " + serialPort1.PortName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnLedToggle_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                MessageBox.Show("Please connect first.");
+                return;
+            }
+
+            if (ledState == false)
+            {
+                serialPort1.WriteLine("ON");
+                btnLedToggle.Text = "Turn LED OFF";
+                ledState = true;
+            }
+            else
+            {
+                serialPort1.WriteLine("OFF");
+                btnLedToggle.Text = "Turn LED ON";
+                ledState = false;
+            }
+        }
+
+        private void trackBarDAC_Scroll(object sender, EventArgs e)
+        {
+            int percent = trackBarDAC.Value; // مقدار بین 0 تا 100
+            lblDACValue.Text = percent.ToString() + " %";
+
+            if (serialPort1.IsOpen)
+            {
+                // دستور رو به صورت "DAC:50" می‌فرسته
+                serialPort1.WriteLine("DAC:" + percent.ToString());
+            }
+        }
+
+    }
+}
+
